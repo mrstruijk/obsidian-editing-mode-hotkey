9c0986b6363e1a4eb6d111fc5ee7ed0523225d54
mrstruijk <mrstruijk@gmail.com>
2025-03-13 13:35:23 +0100

    First draft of cycling reading / live preview / editing

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import {
 	Plugin,
+	MarkdownView
 } from 'obsidian';
 
 export default class EditingModeHotkey extends Plugin {
@@ -8,7 +9,7 @@ export default class EditingModeHotkey extends Plugin {
 
 		this.addCommand({
 			id: 'toggleDefaultEditingMode',
-			name: 'Toggle default editing mode (Source/Live Preview)',
+			name: 'Toggle modes (Source/Live Preview/Reading)',
 			hotkeys: [
 				{
 					modifiers: ['Mod', 'Shift'],
@@ -22,28 +23,44 @@ export default class EditingModeHotkey extends Plugin {
 	private toggleDefaultEditingMode() {
 		// check the curren default view mode
 		const livePreview = this.app.vault.getConfig("livePreview");
-
-		// set the view mode of all tabs to either source or live preview
-		if (livePreview === false) {
-			this.app.vault.setConfig("livePreview", true);
-			this.app.workspace.iterateAllLeaves(leaf => {
-				const view = leaf.getViewState();
-				// check if the current view mode is in edit view, to prevent the state of tabs such as kanban boards to be changed
-				if (view.state.mode === 'source') {
-					view.state.source = false;
-					leaf.setViewState(view);
-				}
-			});
-		} else if (livePreview === true) {
+		
+		const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+		const isEditing = view?.getMode() === "source";
+		const isReading = view?.getMode() === "preview";
+		
+		if (isReading === true) {
 			this.app.vault.setConfig("livePreview", false);
-			this.app.workspace.iterateAllLeaves(leaf => {
-				const view = leaf.getViewState();
-				// check if the current view mode is in edit view, to prevent the state of tabs such as kanban boards to be changed
-				if (view.state.mode === 'source') {
-					view.state.source = true;
-					leaf.setViewState(view);
-				}
-			});
+				this.app.workspace.iterateAllLeaves(leaf => {
+					const view = leaf.getViewState();
+					if (view.state.mode === 'preview') {
+						view.state.mode = 'source';
+						view.state.source = true;
+						leaf.setViewState(view);
+					}
+				});
+		} else if (isEditing === true) {
+			if (livePreview === false) {
+				this.app.vault.setConfig("livePreview", true);
+				this.app.workspace.iterateAllLeaves(leaf => {
+					const view = leaf.getViewState();
+					// check if the current view mode is in edit view, to prevent the state of tabs such as kanban boards to be changed
+					if (view.state.mode === 'source') {
+						view.state.source = false;
+						leaf.setViewState(view);
+					}
+				});
+			} else if (livePreview === true) {
+				this.app.vault.setConfig("livePreview", false);
+				this.app.workspace.iterateAllLeaves(leaf => {
+					const view = leaf.getViewState();
+					// check if the current view mode is in edit view, to prevent the state of tabs such as kanban boards to be changed
+					if (view.state.mode === 'source') {
+						view.state.mode = 'preview';
+						view.state.source = false;
+						leaf.setViewState(view);
+					}
+				});
+			}
 		}
 	}
 }
